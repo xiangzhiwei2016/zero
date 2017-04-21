5f1d5140c16d1b9c9d90c08f39357769f2dfe784
xzw111 <740179133@qq.com>
2017-04-21 18:08:40 +0800

    项目名修改

--- src/main/java/com/dao/impl/OperateLogDaoImpl.java
+++ src/main/java/com/dao/impl/OperateLogDaoImpl.java
@@ -18,33 +18,33 @@ import com.mchange.v1.util.IteratorUtils;
 import com.repository.OperateLogTestRepository;
 
 @Named("operateLogDao")
-public class OperateLogDaoImpl implements OperateLogDao{
+public class OperateLogDaoImpl implements OperateLogDao {
 	private Logger logger = LoggerFactory.getLogger(OperateLogDaoImpl.class);
-	
-	@Inject 
+
+	@Inject
 	OperateLogTestRepository operateLogTestRepository;
-	
+
 	@Inject
 	EntityManager entityManager;
-	
+
 	public List<OperateLogTest> test() {
-		 logger.info("sdfsdfsfsf");
-		 Iterator<OperateLogTest> list = operateLogTestRepository.findAll().iterator();
-		 List<OperateLogTest> ls = IteratorUtils.toArrayList(list, 0);
-		 logger.info("±£¥Ê");
-		 OperateLogTest o = new OperateLogTest("1","2","1","2","1","2","1","2","1","2");
-//		 OperateLogTest o2 = operateLogTestRepository.save(o);
-//		 System.out.println(ls.size()+o2.toString());
-		 Query query = entityManager.createQuery("select t from OperateLogTest t where t.id=22 ",OperateLogTest.class);
-			List result = query.getResultList();
-			if (result != null) {
-				Iterator iterator = result.iterator();
-				while (iterator.hasNext()) {
-					OperateLogTest test = (OperateLogTest) iterator.next();
-					logger.info(test.toString());
-				}
+		logger.info("OperateLogDaoImpl≤‚ ‘");
+
+		logger.info("±£¥Ê");
+		OperateLogTest o = new OperateLogTest("1", "2", "1", "2", "1", "2", "1", "2", "1", "2");
+		OperateLogTest o2 = operateLogTestRepository.save(o);
+		Query query = entityManager.createQuery("select t from OperateLogTest t ", OperateLogTest.class);
+		List result = query.getResultList();
+		if (result != null) {
+			Iterator iterator = result.iterator();
+			while (iterator.hasNext()) {
+				OperateLogTest test = (OperateLogTest) iterator.next();
+				logger.info(test.toString());
 			}
-		 return ls;
+		}
+		Iterator<OperateLogTest> list = operateLogTestRepository.findAll().iterator();
+		List<OperateLogTest> ls = IteratorUtils.toArrayList(list, 0);
+		return ls;
 	}
 
 }
